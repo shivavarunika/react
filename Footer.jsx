26e055278b9f3d219d201d4bd823cd84fb63305e
shivavarunika <136820789+shivavarunika@users.noreply.github.com>
2025-03-26 15:24:53 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer(){
+return(
+    <p style={{textAlign:"center", backgroundColor: "brown", height:"100"}}>This is ti tell its created on own and no copyright</p>
+)
+}
---
+++ WebsiteLink.jsx
@@ -0,0 +1,21 @@
+export default function WebsiteLink() {
+    return (
+      <>
+        <div>
+        <ul>
+          <img
+            src="https://cdn.britannica.com/22/187222-050-07B17FB6/apples-on-a-tree-branch.jpg" alt="Apple" width={300} height={300} />
+            <li>apple</li>
+          <img
+            src="https://cdn.britannica.com/24/174524-050-A851D3F2/Oranges.jpg" alt="Orange"width={300} height={300}
+          />
+            <li>orange</li>
+          <img
+            src="https://images.indianexpress.com/2021/02/grapes-1200.jpg" alt="Grapes" width={300} height={300}
+          />
+         <li>grapes</li>
+          </ul>
+        </div>
+      </>
+    );
+  }
